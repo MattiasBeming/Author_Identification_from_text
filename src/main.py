728f52587c0e2d8e8b39941e0a7032e27a986b45
Mattias Beming <mattias.beming@outlook.com>
2021-12-19 19:10:30 +0100

    feat[wip]: first code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from storage import *
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("../")
+    main()
---
+++ src/storage.py
@@ -0,0 +1,82 @@
+
+import numpy as np
+import tensorflow as tf
+from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import TextVectorization
+from keras.backend import clear_session
+import matplotlib.pyplot as plt
+
+# Reset weights of last training
+clear_session()
+
+filepath = Path(
+    'data/victorian_era/dataset/Gungor_2018_VictorianAuthorAttribution_data-train.csv')
+
+data = pd.read_csv(filepath, encoding="ISO-8859-1")
+
+train, test = train_test_split(data, test_size=0.2)
+
+# Bad rows ? 51082
+# print(data['text'].iloc[51082])
+
+
+# Code from https://keras.io/examples/nlp/text_classification_from_scratch/
+
+
+if False:
+    def custom_standardization(input_data):
+        return input_data
+
+    # Model constants
+    max_features = 20000
+    embedding_dim = 128
+    sequence_length = 1000
+
+    # positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams
+
+    # ngrams - Optional specification for ngrams to create from the possibly-split input text
+
+    vectorize_layer = TextVectorization(
+        # standardize=custom_standardization,
+        max_tokens=max_features,
+        output_mode="int",
+        output_sequence_length=sequence_length,
+    )
+
+    # embedding_layer : input_dim (summation of unique words in your corpus)
+    # output_dim (size of corresponding dense vectors)
+    # input_length (length of input sequences)
+
+BATCH_SIZE = 64
+EPOCHS = 100
+
+if False:
+    plt.style.use('ggplot')
+    history = model.fit(X_train, y_train,
+                        epochs=EPOCHS,
+                        verbose=False,
+                        validation_data=(X_test, y_test),
+                        batch_size=BATCH_SIZE)
+
+    def plot_history(history):
+        acc = history.history['acc']
+        val_acc = history.history['val_acc']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        x = range(1, len(acc) + 1)
+
+        plt.figure(figsize=(12, 5))
+        plt.subplot(1, 2, 1)
+        plt.plot(x, acc, 'b', label='Training acc')
+        plt.plot(x, val_acc, 'r', label='Validation acc')
+        plt.title('Training and validation accuracy')
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(x, loss, 'b', label='Training loss')
+        plt.plot(x, val_loss, 'r', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.legend()
+
+    plot_history(history)
